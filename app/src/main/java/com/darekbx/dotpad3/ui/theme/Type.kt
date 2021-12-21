2a78dc3717f2f958439c14f7d54cc30205348bce
darekbx <darek.baranczuk@gmail.com>
2021-12-21 09:35:24 +0100

    Add dialog message focus

--- app/src/main/java/com/darekbx/dotpad3/ui/theme/Type.kt
+++ app/src/main/java/com/darekbx/dotpad3/ui/theme/Type.kt
@@ -24,11 +24,5 @@ val Typography = Typography(
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp,
         color = Color.White
-    ),
-    h4 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        color = Color.White
     )
 )
